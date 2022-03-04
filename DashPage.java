121f255eb6b92fa9b7a2aed50701c011d1334eb2
KayCewu <70000550+KayCewu@users.noreply.github.com>
2022-03-04 13:13:56 +0200

    Update DashPage.java

--- DashPage.java
+++ DashPage.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class DashPage extends AppCompatActivity {
+    //Variable declarations, below are the certain buttons we will be using
     Button btnBack, btnPolice,btnAmbulance,btnFire,btnViolence;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +55,7 @@ public class DashPage extends AppCompatActivity {
         Intent intent = new Intent(this, Login.class);
         startActivity(intent);
     }
+    //This function calls on the CurrLocation class to send the user an alert
     public void CallHelp() {
         Intent intent = new Intent(this, CurrLocation.class);//MyCurrLocation
         startActivity(intent);
